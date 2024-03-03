4ccb645aeb9b04570dd362971f5840c23cea9301
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-03-03 20:16:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,32 @@
 //your code here!
 
+//your code here!
+let infiList=document.querySelector('#infi-list')
+let count=10;
+var k=1;
+addListItem(count)
+
+function addListItem(count)
+{
+    for(let i=1;i<=count;i++)
+    {
+        const listitem=document.createElement('li')
+        listitem.textContent="Item "+(1+infiList.childElementCount);
+        infiList.appendChild(listitem)
+    }
+  
+}
+
+infiList.addEventListener("scroll",()=>
+{   
+    const scrollHeight=document.documentElement.scrollHeight;
+    const scrollTop=document.documentElement.scrollTop;
+    const clientHeight=document.documentElement.clientHeight;
+   
+    if( scrollHeight <= scrollTop+ clientHeight )
+   {
+    // console.log("bottom")
+    addListItem(2);
+    
+   }
+})
